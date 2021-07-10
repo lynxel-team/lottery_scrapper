3ba970de5d06f8b929a66d7eef0768d63cfb114f
liuver-core <liuver.carrera@coresolutions.es>
2021-07-10 16:05:38 -0400

    fix url del scrapper

--- app/Http/Controllers/ParserController.php
+++ app/Http/Controllers/ParserController.php
@@ -22,14 +22,14 @@ class ParserController
 
         $configP3 = [
             'type'                  => 'p3',
-            'url'                   => 'http://crawler.test/p3.htm',
-//        'url'                   => 'https://www.flalottery.com/exptkt/p3.htm',
+//            'url'                   => 'http://crawler.test/p3.htm',
+        'url'                   => 'https://www.flalottery.com/exptkt/p3.htm',
             'updateCount'           => 3,           //Cantidad de tablas a actualizar [0]-> Todas
         ];
         $configP4 = [
             'type'                  => 'p4',
-            'url'                   => 'http://crawler.test/p4.htm',
-//        'url'                   => 'https://www.flalottery.com/exptkt/p4.htm',
+//            'url'                   => 'http://crawler.test/p4.htm',
+        'url'                   => 'https://www.flalottery.com/exptkt/p4.htm',
             'updateCount'           => 4,           //Cantidad de tablas a actualizar [0]-> Todas
         ];
         \App\Jobs\ParseNumberPage::withChain([
