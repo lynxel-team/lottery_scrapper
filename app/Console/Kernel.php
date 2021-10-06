5a1a223ba6e69ed06a0255d639da61c8ac82c50b
liuver-core <liuver.carrera.espinosa@gmail.com>
2021-10-06 15:55:01 -0400

    update numbers command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\UpdateNumbersCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
+        UpdateNumbersCommand::class,
     ];
 
     /**
@@ -24,7 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('numbers:update')
+            ->dailyAt('1:00');
     }
 
     /**
